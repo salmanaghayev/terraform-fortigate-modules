5b51188a1956619647215d2b49d24d4c242710c0
Salman Aghayev <salman.aghayev@gmail.com>
2024-11-08 14:04:21 -0500

    Added Policy Module

--- firewall-policy/outputs.tf
+++ firewall-policy/outputs.tf
@@ -1,1 +1,13 @@
 
+output "firewall_policy" {
+  description = "A map of all firewall policies' with their details"
+  value = {
+    for name, policy in fortios_firewall_policy.firewall_policy :
+    name => {
+      name     = policy.name
+      id       = policy.id
+      src_addr = policy.src_addr
+      dst_addr = policy.dst_addr
+    }
+  }
+}
